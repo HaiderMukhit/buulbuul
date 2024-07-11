04fd4ea38429e0b3c0584926340e5f92fe1426aa
haidermukhit <haider.mukhit@gmail.com>
2024-07-12 00:16:09 +0200

    design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,19 @@ const typography = require('@tailwindcss/typography');
 
 module.exports = {
 	content: ['./hugo_stats.json'],
+
+	theme: {
+
+		extend: {
+
+			fontFamily: {
+			
+				'kumar': ['Kumar One'],
+			},
+
+		}
+	},
+
 	plugins: [typography],
 };
+
